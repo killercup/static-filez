38e926ce36600ee6b0b58604ada9532c5a5ed204
Pascal Hertleif <killercup@gmail.com>
2018-10-20 19:39:41 +0200

    Collect files, then sort vector
    It's noticeably slower, but this is the generating step. It should need
    to be *super* fast.

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,9 @@ extern crate mime_guess;
 extern crate quicli;
 extern crate tokio;
 extern crate walkdir;
+#[cfg(test)]
+#[macro_use]
+extern crate proptest;
 
 use std::fs::File;
 use std::path::Path;
@@ -42,7 +45,7 @@ pub fn build(src: &Path, target: &Path) -> Result<(), Error> {
         File::create(&index_path)
             .with_context(|e| format!("couldn't create file `{}`: {}", target.display(), e))?,
     );
-    let index = fst::MapBuilder::new(index)
+    let mut index = fst::MapBuilder::new(index)
         .with_context(|e| format!("couldn't create index file `{}`: {}", target.display(), e))?;
     info!("will write index to `{}`", index_path.display());
 
@@ -55,7 +58,22 @@ pub fn build(src: &Path, target: &Path) -> Result<(), Error> {
 
     let mut archive_index = 0;
 
-    fn rel_as_bytes<'a>(p: &'a DirEntry, src: &Path) -> Vec<u8> {
+    let mut files = WalkDir::new(src)
+        .into_iter()
+        .par_bridge()
+        .flat_map(|entry| entry.map_err(|e| warn!("Couldn't read dir entry {}", e)))
+        .filter(|f| f.path().is_file())
+        .collect::<Vec<_>>();
+
+    ensure!(
+        !files.is_empty(),
+        "Would create empty archive. Is the `{}` directory empty?",
+        src.display()
+    );
+    info!("found {} files", files.len());
+
+    // fst map requires keys to be inserted in lexicographic order _represented as bytes_
+    fn rel_as_bytes(p: &DirEntry, src: &Path) -> Vec<u8> {
         p.path()
             .strip_prefix(src)
             .unwrap()
@@ -63,40 +81,44 @@ pub fn build(src: &Path, target: &Path) -> Result<(), Error> {
             .to_string()
             .into_bytes()
     }
+    files.sort_by(move |a, b| rel_as_bytes(a, src).cmp(&rel_as_bytes(b, src)));
+    info!("sorted {} files", files.len());
 
-    WalkDir::new(src)
-        .sort_by(move |a, b| rel_as_bytes(a, src).cmp(&rel_as_bytes(b, src)))
-        .contents_first(true)
-        .into_iter()
-        .flat_map(|entry| entry.map_err(|e| warn!("Couldn't read dir entry {}", e)))
-        .filter(|f| f.path().is_file())
-        .try_fold(index, |mut map, file| -> Result<_, Error> {
-            let path = file.path();
-            trace!("trying to add {} to index", path.display());
-            let file_content = get_compressed_content(&path).with_context(|_| {
-                format!("Could not read/compress content of {}", path.display())
-            })?;
-            archive.write_all(&file_content).with_context(|_| {
-                format!(
-                    "Could not write compressed content to {}",
-                    archive_path.display()
-                )
-            })?;
+    info!(
+        "now building archive {} as well as index {}",
+        archive_path.display(),
+        index_path.display()
+    );
+    for file in &files {
+        let path = file.path();
+        let file_content = get_compressed_content(&path)
+            .with_context(|_| format!("Could not read/compress content of {}", path.display()))?;
+        archive.write_all(&file_content).with_context(|_| {
+            format!(
+                "Could not write compressed content to {}",
+                archive_path.display()
+            )
+        })?;
 
-            let rel_path = file
-                .path()
-                .strip_prefix(src)
-                .with_context(|_| format!("Couldn't get relative path for `{:?}`", path.display()))?
-                .to_path_buf();
+        let rel_path = file
+            .path()
+            .strip_prefix(src)
+            .with_context(|_| format!("Couldn't get relative path for `{:?}`", path.display()))?
+            .to_path_buf();
 
-            map.insert(
+        index
+            .insert(
                 rel_path.to_string_lossy().as_bytes(),
                 slice::pack_in_u64(archive_index, file_content.len()),
             ).with_context(|_| format!("Could not insert file {} into index", path.display()))?;
-            archive_index += file_content.len();
-            Ok(map)
-        })?.finish()
+        archive_index += file_content.len();
+    }
+    info!("wrote all files");
+
+    index
+        .finish()
         .with_context(|e| format!("Could not finish building index: {}", e))?;
+    info!("finished index");
 
     Ok(())
 }
